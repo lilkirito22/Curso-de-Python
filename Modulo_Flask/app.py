35041a23fa67758669e761e8755e9cd4d0f07394
Daniel Chaves <85415599+lilkirito22@users.noreply.github.com>
2024-02-27 16:03:42 -0300

    Add CRUD functionality and new route for cars

--- Modulo_Flask/app.py
+++ Modulo_Flask/app.py
@@ -2,6 +2,18 @@ from flask import Flask
 
 app= Flask(__name__)
 
+
+#CRUD
+#Create, Read, Update and Delete 
+
 @app.route("/")
 def hello_world():
-  print("ola Mundo")
+  return "Ola Mundo"
+
+@app.route("/cars")
+def cars():
+  return "Formula 1"
+if __name__ == "__main__":
+  app.run(debug=True)
+
+  
