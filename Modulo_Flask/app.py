b7d78754f9a41e90e71842da711b5f25e95cecb2
Daniel Chaves <85415599+lilkirito22@users.noreply.github.com>
2024-02-28 17:53:53 -0300

    Add PUT endpoint to update task

--- Modulo_Flask/app.py
+++ Modulo_Flask/app.py
@@ -41,6 +41,26 @@ def get_task(id):
       
    return jsonify({"message": "Não foi possivel encontrar essa a atividade" }), 404
 
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+   task = None
+   for t in tasks:
+      if t.id==id:
+         task = t
+
+   print(task)
+
+   if task == None:
+      return jsonify({"message": "Não foi possivel encontrar essa a atividade" }), 404
+   
+   data = request.get_json()      
+   task.title = data['title']
+   task.description = data['description']
+   task.completed = data['completed']
+   print(task)
+   return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
 if __name__ == "__main__":
   app.run(debug=True)
 
